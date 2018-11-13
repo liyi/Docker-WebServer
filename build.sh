6116543f010a37affa146ce47e7db3a5512fd8db
liyi <liyi1987@qq.com>
2018-11-14 00:45:53 +0800

    添加php5服务器

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 # 构建镜像
+${SUDO} docker build --no-cache -t ${NAMESPACE}/php5 ./php5
 ${SUDO} docker build --no-cache -t ${NAMESPACE}/php7 ./php7
 ${SUDO} docker build --no-cache -t ${NAMESPACE}/nginx ./nginx
 
